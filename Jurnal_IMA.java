0f718938f73493f433cbdf3cad57c9c59e9d55eb
Rafi Hidayat <rafi.hidayat1313@gmail.com>
2023-09-27 13:48:30 +0700

    Jurnal IMA

---
+++ Jurnal_IMA.java
@@ -0,0 +1,7 @@
+public class Jurnal_IMA {    
+    public static void main(String[] args) {
+        System.out.println("Nama    : Rafi Hidayat");
+        System.out.println("NIM     : 607062330092");
+        System.out.println("Kelas   : D3IF-47-01");
+    }
+}
