c3a9a4b5854f26e76040cabfeadcbc59cebf23f4
Rafi Hidayat <rafi.hidayat1313@gmail.com>
2023-09-27 13:29:07 +0700

    Tugas IMA

---
+++ Tugas IMA.java
@@ -0,0 +1,7 @@
+class Jurnal01{    
+    public static void main(String[] args) {
+        System.out.println("Nama    : Rafi Hidayat");
+        System.out.println("NIM     : 607062330092");
+        System.out.println("Kelas   : D3 RPLA - 01");
+    }
+}
--- public class Jurnal01{.java
+++
@@ -1,5 +0,0 @@
-class Jurnal01{    
-    public static void main(String[] args) {
-        System.out.println("HELLO WORLD\nyaes");
-    }
-}
